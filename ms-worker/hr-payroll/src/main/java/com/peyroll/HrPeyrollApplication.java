aeec74f592e9b91bb775b4773184e2d6f9a09a38
AndreLuvetama <andreluares30@gmail.com>
2022-12-16 11:30:16 -0300

    PRJ HR-Payroll created

---
+++ ms-worker/hr-payroll/src/main/java/com/peyroll/HrPeyrollApplication.java
@@ -0,0 +1,13 @@
+package com.peyroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPeyrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPeyrollApplication.class, args);
+	}
+
+}
---
+++ ms-worker/hr-payroll/src/test/java/com/peyroll/HrPeyrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peyroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPeyrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
